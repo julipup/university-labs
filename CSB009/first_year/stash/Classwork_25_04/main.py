24ec20764edd2431ad3a0cb650b6918e9daea5f9
SniperFox213 <dingolediego@gmail.com>
2022-10-12 21:04:07 +0300

    Update repo structure

---
+++ CSB009/first_year/stash/Classwork_25_04/main.py
@@ -0,0 +1,13 @@
+import search
+import sort
+
+if __name__ == "__main__":
+  print('Search module:')
+  print(search.linear_search([1, 2, 3, 4, 5], 4))
+  print(search.binary_search([1, 2, 3, 4, 5, 6], 4))
+  print(search.substring_search("Hello there!", "nope"))
+
+  print('Sort module:')
+  print(sort.bubble_sort([5, 4, 3, 2, 1]))
+  print(sort.selection_sort([5, 4, 3, 2, 1]))
+  print(sort.insertion_sort([5, 4, 3, 2, 1]))
